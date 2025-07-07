b5dea90bf5f66f400af34d43530fbc7fd91f4567
Md Abu Rayhan <47344727+AbuRayhanRU@users.noreply.github.com>
2025-07-07 21:08:03 +0600

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class=""></i> Home</a>
+         <a href="about.php"> <i class=""></i> About</a>
+         <a href="shop.php"> <i class=""></i> Shop</a>
+         <a href="contact.php"> <i class=""></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class=""></i> Login</a>
+         <a href="user_register.php"> <i class=""></i> Register</a>
+         <a href="cart.php"> <i class=""></i> Cart</a>
+         <a href="orders.php"> <i class=""></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +91 9876543210</a>
+         
+         <a href="mailto:groupofwebservices@gmail.com"><i class="fas fa-envelope"></i> groupofwebservices@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> INDIA </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/gwstutor" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         
+         <a href="https://www.instagram.com/gwstutor/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         </a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>GWS Tutor</span> | all rights reserved!</div>
+
+</footer>
